d76b1086ec7748f10d0948186ce72d7726151f25
Mahsa Bazzaz <mahsa.bazzaz@gmail.com>
2018-07-05 22:34:32 +0430

    Bullet added.

---
+++ src/Bullet.java
@@ -0,0 +1,12 @@
+import java.awt.image.BufferedImage;
+
+public class Bullet {
+    private int damage;
+    private BufferedImage image;
+    private int XPosition;
+    private int YPosition;
+    private final int BULLET_SPEED = 0;
+    public Bullet(){
+
+    }
+}
